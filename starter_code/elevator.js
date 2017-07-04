e0a8c941a899b1fd933d2539959357664413c113
Alejandro de la Fuente <alex695afr@gmail.com>
2017-07-04 15:11:04 +0200

    Commit

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,53 @@
+/* jshint esversion:6 */
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.waiting = [];
+    this.passengers = [];
+    this.direction = 0;
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+
+    this.cont = setInterval(() => this.update(), 1000);
+  }
+  stop() {
+    clearInterval(this.cont);
+  }
+  update() {
+
+    this.floorUp();
+    this.log();
+      
+
+  }
+  _passengersEnter() {}
+  _passengersLeave() {}
+
+
+  floorUp() {
+    this.floor++;
+    if (this.floor > this.MAXFLOOR) {
+      this.floor = this.MAXFLOOR;
+    }
+  }
+  floorDown() {
+    this.floor--;
+    if (this.floor < 0) {
+      this.floor = 0;
+    }
+  }
+
+  call(person) {
+    this.requests.push(person);
+    console.log(this.requests);
+  }
+
+  log() {
+    console.log(`Floor : ${this.floor} | direction : ${this.direction}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,7 @@
+/* jshint esversion:6*/
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+let elevator = new Elevator();
+let juan = new Person('Juan', 0, 2);
+elevator.start();
+elevator.call(juan);
